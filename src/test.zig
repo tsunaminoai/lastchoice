fe64dce93f695a3c2d596648d6dd686e251d3545
Tsunami Noai <spam@falseblue.com>
2023-10-08 20:22:17 -0400

    get closer to understanding text encoding

--- src/test.zig
+++ src/test.zig
@@ -28,6 +28,11 @@ const Form = struct {
     pub fn deinit(self: *Form, allocator: std.mem.Allocator) void {
         allocator.free(self.data);
     }
+    pub fn logHex(self: *Form) void {
+        for (self.data) |c| {
+            if (c != 0) std.log.debug("{X}", .{c});
+        }
+    }
 };
 
 const Record = struct {
@@ -77,16 +82,18 @@ pub fn main() anyerror!void {
                 const lines = std.mem.readIntSliceBig(u16, block.blockData[4..6]);
                 // const len = std.mem.readIntSliceBig(u16, block.blockData[2..4]);
                 form = try Form.init(lines, 120 + numBlocks * 128, alloc);
-                std.mem.copy(u8, form.data, &block.blockData);
-                for (1..numBlocks) |j| {
-                    std.mem.copy(u8, form.data[120 + (j - 1) * 128 ..], &blockList.items[i + j].blockData);
+                std.mem.copy(u8, form.data[0..120], block.blockData[6..]);
+                for (0..numBlocks) |j| {
+                    var idx = 120 + (j) * 128;
+                    std.log.debug("IDX for form data insertion: {}\n", .{idx});
+                    std.mem.copy(u8, form.data[idx..], &blockList.items[i + j].blockData);
                 }
             },
             .FormDescriptionContinuation => continue,
             .DataRecord => {
                 const numBlocks = std.mem.readIntSliceLittle(u16, block.blockData[0..2]);
                 var data = try alloc.alloc(u8, numBlocks * 128);
-                defer alloc.free(data);
+                // defer alloc.free(data);
 
                 for (0..numBlocks) |j| {
                     //todo: remember theres an extra byte in the first block that needs removed
@@ -106,64 +113,154 @@ pub fn main() anyerror!void {
     defer form.deinit(alloc);
     std.log.debug("{any}\n", .{head});
     // std.log.debug("{any} {} \n", .{ recordList, recordList.items.len });
+    // form.logHex();
 
-    std.log.debug("{any}\n", .{form.data});
+    // var D = recordList.items[2];
+    var D = form;
+
     var i: usize = 0;
 
+    const Field = struct {
+        fieldType: u8,
+        fieldAttributes: CharacterAttributes = CharacterAttributes{},
+    };
     var charList = std.ArrayList(TextChar).init(alloc);
+    var fieldList = std.ArrayList(Field).init(alloc);
     defer charList.deinit();
+    defer fieldList.deinit();
 
-    while (i < form.data.len) {
-        var char = form.data[i];
-        var newChar = TextChar{ .character = char, .attributes = ChatacterAttributeMask{} };
-        switch (char) {
-            0x0, 0x7F => {
-                //todo: plain char
-                i += 1;
-            },
-            else => |c| {
-                // handle multibyte
-                newChar.character = c & 0x7F;
-                const byte2 = form.data[i + 1];
-                switch (byte2) {
-                    0xD0, 0xDF => {
-                        //need 3rd byte for background text or field text
-                        const byte3 = form.data[i + 2];
-                        newChar.setAtttributes(byte3);
+    full: while (i < D.data.len - 3) {
+        //get size of text
+        var size = std.mem.readIntSliceLittle(u16, D.data[i .. i + 2]);
+        if (size == 0) break :full;
+        i += 2;
+        std.log.debug("size: {}\n", .{size});
 
-                        i += 3;
-                    },
-                    0x81, 0x8F => {
-                        //normal text
-                        i += 2;
-                    },
-                    0x90, 0x9F => {
-                        //field names
-                        i += 2;
-                    },
-                    else => {
-                        //todo
-                        i += 2;
-                    },
-                }
-            },
+        var j: usize = 0;
+        while (j < size) {
+            if (i + j >= D.data.len) break :full;
+            const char = D.data[i + j];
+            var newChar = TextChar{};
+            switch (char) {
+                0x0, 0x7F => {
+                    // plain character, nothing more to do
+                    j += 1;
+                },
+                0x0d => {
+                    newChar.character = '\n';
+                    j += 1;
+                },
+                else => |c| {
+                    // handle multibyte
+                    newChar.character = c & 0x7F;
+                    const byte2 = D.data[i + j + 1];
+                    switch (byte2) {
+                        0xD0, 0xDF => {
+                            //need 3rd byte for background text or field text
+                            const byte3 = D.data[i + j + 2];
+                            newChar.setAtttributes(byte2);
+                            newChar.setAtttributes(byte3);
+
+                            j += 3;
+                        },
+                        0x81, 0x8F => {
+                            //normal text but with attrs
+                            newChar.setAtttributes(byte2);
+                            j += 2;
+                        },
+                        0x90, 0x9F => {
+                            std.log.debug("Two byte: char:{c} stripped: {c}\n", .{ c, newChar.character });
+                            if (FieldTypeTag.isValid(c)) {
+                                var tag = @as(FieldTypeTag, @enumFromInt(c));
+                                std.log.info("Field Type: {s}\n", .{@tagName(tag)});
+                            }
+                            //field names
+                            newChar.setAtttributes(byte2);
+
+                            j += 2;
+                        },
+                        0xC0, 0xCF => {
+                            //need 3rd byte for normal text
+                            const byte3 = D.data[i + j + 2];
+                            newChar.setAtttributes(byte2);
+                            newChar.setAtttributes(byte3);
+
+                            j += 3;
+                        },
+                        else => |x| {
+                            //todo
+                            std.log.warn("Found a weird byte: {X}: '{c}'", .{ x, x });
+                            j += 2;
+                        },
+                    }
+                },
+            }
+            // std.log.debug("{}, {any}\n", .{ i, newChar });
+            try charList.append(newChar);
         }
-        try charList.append(newChar);
+
+        i += j;
     }
 
     std.log.debug("Read in {} chars\n", .{charList.items.len});
+    for (charList.items) |item| {
+        std.debug.print("{c}", .{item.character});
+    }
+    std.log.debug("My record: {s}\n", .{std.fmt.fmtSliceEscapeUpper(D.data[0..126])});
+    for (D.data) |x| {
+        std.debug.print("{c}", .{x & 0x7F});
+    }
+    std.debug.print("\n", .{});
+
+    std.log.debug(
+        \\Size: {},{}
+        \\String: {c}
+        \\more1: {}
+        \\spmething: {X}
+    , .{
+        D.data[0],                         D.data[1],
+        D.data[2..3].*[0] & @as(u8, 0x7F), std.mem.readIntSliceBig(u16, D.data[3..5]),
+        D.data[3],
+        // std.fmt.bytesToHex(D.data[5..77], .upper),
+        // std.mem.readIntSliceBig(u16, D.data[24..26]),
+    });
+    for (recordList.items) |*record| {
+        alloc.free(record.data);
+    }
 }
 
-const ChatacterAttributeMask = packed struct(u8) {
+const FieldTypeTag = enum(u8) {
+    General = ' ',
+    Numeric = 'N',
+    Date = 'D',
+    Time = 'T',
+    Bool = 'Y',
+
+    pub fn isValid(char: u8) bool {
+        const fields = comptime @typeInfo(FieldTypeTag).Enum.fields;
+        inline for (fields) |f| {
+            if (char == f.value) return true;
+        }
+        return false;
+    }
+};
+
+test "fieldtype" {
+    try std.testing.expect(FieldTypeTag.isValid('N') == true);
+    try std.testing.expect(FieldTypeTag.isValid('A') == false);
+}
+
+const ScriptState = enum { Normal, Super, Sub };
+const CharacterAttributes = struct {
     underline: bool = false,
     bold: bool = false,
     italic: bool = false,
-    _more: u5 = 0,
+    script: ScriptState = .Normal,
 };
 
 const TextChar = struct {
     character: u8 = 0,
-    attributes: ChatacterAttributeMask,
+    attributes: CharacterAttributes = CharacterAttributes{},
 
     const Self = @This();
 
@@ -176,9 +273,25 @@ const TextChar = struct {
     pub fn setUnderline(self: *Self, checkByte: u8) void {
         self.attributes.bold = checkByte & 0x1 == 0x1;
     }
+    pub fn setSuper(self: *Self, checkByte: u8) void {
+        if (checkByte & 0xFE == 0x84) {
+            self.attributes.script = .Super;
+        } else {
+            self.attributes.script = .Normal;
+        }
+    }
+    pub fn setSub(self: *Self, checkByte: u8) void {
+        if (checkByte & 0xFE == 0x82) {
+            self.attributes.script = .Sub;
+        } else {
+            self.attributes.script = .Normal;
+        }
+    }
     pub fn setAtttributes(self: *Self, maskByte: u8) void {
         self.setBold(maskByte);
         self.setItalic(maskByte);
         self.setUnderline(maskByte);
+        self.setSuper(maskByte);
+        self.setSub(maskByte);
     }
 };
